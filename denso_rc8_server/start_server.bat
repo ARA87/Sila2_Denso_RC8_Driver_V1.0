6ec365b91e8bd3b353e71c0d69c2db44581f954a
ARA87 <160502151+ARA87@users.noreply.github.com>
2025-10-10 15:49:35 +0200

    Update start_server.bat

--- denso_rc8_server/start_server.bat
+++ denso_rc8_server/start_server.bat
@@ -1,4 +1,5 @@
-cd C:\your_path\Sila2_Denso_RC8_Driver_V1.2-main
-python -m sila_driver.denso_rc8_server --ip-address 0.0.0.0 --port 50052
+cd C:\"your_path"\Sila2_Denso_RC8_Driver_V1.0-main
+python -m denso_rc8_server --ip-address 0.0.0.0 --port 50052
 
 pause
+
